8e0ba473b8ddc31162725e6f2715c19a044c303b
Ivan Delic <ivan.delic@oracle.com>
2022-09-28 00:12:35 +0200

    Implementing Fn context variables

--- func.js
+++ func.js
@@ -1,11 +1,12 @@
 const fdk = require('@fnproject/fdk');
 const https = require('https');
 
-fdk.handle(async function (input) {
-  return httpsRequest({url: 'https://login.microsoftonline.com/common/discovery/keys'}).then(data => {
+fdk.handle(async function (input, ctx) {
+  return httpsRequest({url: ctx.config["AZURE_JWKS_URI"]}).then(data => {
     data.keys.forEach(element => {
       element["alg"] = "RS256"
     });
+    data.keys = data.keys.slice(ctx.config["AZURE_JWKS_SLICE_START"], ctx.config["AZURE_JWKS_SLICE_END"])
     console.log("Modified JWKS:" + JSON.stringify(data));
     return data;
   }, error => {
